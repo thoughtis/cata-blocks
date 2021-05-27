2648a6fdb48d654cf768c9982b42828e29cbe8d6
Douglas Johnson <doug@thoughtcatalog.com>
2021-05-27 15:33:48 -0400

    small fontSize for kicker in trivia pattern

--- patterns/trivia/trivia.php
+++ patterns/trivia/trivia.php
@@ -13,6 +13,6 @@ register_block_pattern(
 		'title'       => __( 'Trivia', 'cata' ),
 		'categories'  => array( 'text' ),
 		'description' => _x( 'Trivia question pattern. An aside containing a title, question and answer.', 'Block pattern description', 'cata' ),
-		'content'     => "<!-- wp:cata/aside -->\n<aside class=\"wp-block-cata-aside\">\n<div class=\"wp-block-cata-aside__inner-container\">\n<!-- wp:cata/kicker -->\n<p class=\"wp-block-cata-kicker\">Trivia Question</p>\n<!-- /wp:cata/kicker -->\n<!-- wp:paragraph -->\n<p><strong>Question:</strong> Question here?</p>\n<!-- /wp:paragraph -->\n<!-- wp:paragraph -->\n<p><strong>Answer:</strong> <span class=\"tap-reveal\">Answer here</span></p>\n<!-- /wp:paragraph -->\n</div>\n</aside>\n<!-- /wp:cata/aside -->\n",
+		'content'     => "<!-- wp:cata/aside -->\n<aside class=\"wp-block-cata-aside\">\n<div class=\"wp-block-cata-aside__inner-container\">\n<!-- wp:cata/kicker {\"fontSize\":\"small\"} -->\n<p class=\"wp-block-cata-kicker has-small-font-size\">Trivia Question</p>\n<!-- /wp:cata/kicker -->\n<!-- wp:paragraph -->\n<p><strong>Question:</strong> Question here?</p>\n<!-- /wp:paragraph -->\n<!-- wp:paragraph -->\n<p><strong>Answer:</strong> <span class=\"tap-reveal\">Answer here</span></p>\n<!-- /wp:paragraph -->\n</div>\n</aside>\n<!-- /wp:cata/aside -->\n",
 	)
 );
