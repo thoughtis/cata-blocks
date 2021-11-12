e40960f017e40c9cdd9c99e459a24704c3f22ee0
Ricardo Dominguez <ricardo@thoughtcatalog.com>
2021-11-12 13:19:31 -0500

    move img inline styles to cata theme css

--- blocks/newsletter/build/index.asset.php
+++ blocks/newsletter/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '740ef1888e10abed23268e778b3eac7b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9d424e2e659acee1cc82e5dbb2b4f868');
