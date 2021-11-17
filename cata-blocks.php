03d61997aa5bff5894df911bc72767eae4d0e645
Douglas Johnson <doug@thoughtcatalog.com>
2021-11-17 12:02:14 -0500

    server side render the newsletter block

--- cata-blocks.php
+++ cata-blocks.php
@@ -25,6 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require_once __DIR__ . '/blocks/aside/aside.php';
 require_once __DIR__ . '/blocks/kicker/kicker.php';
+require_once __DIR__ . '/blocks/newsletter/includes/class-renderer.php';
 require_once __DIR__ . '/blocks/newsletter/newsletter.php';
 require_once __DIR__ . '/blocks/table-of-contents/table-of-contents.php';
 require_once __DIR__ . '/patterns/trivia/trivia.php';
