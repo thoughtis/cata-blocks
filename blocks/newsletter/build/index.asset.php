e8c8d9fb58e1d123f22e8c662a9a95473a93978b
Douglas Johnson <doug@thoughtcatalog.com>
2022-02-03 14:18:02 -0500

    update newsletter

--- blocks/newsletter/build/index.asset.php
+++ blocks/newsletter/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'aada77fa005ab4c752a8b3e83d915b1a');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'fe4132af4b843e12ce46737d89f83844');
--- blocks/newsletter/build/script.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'cef86a791bbc1345f2bac6a83c1f8a4e');
