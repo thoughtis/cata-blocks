6855787dd63ab64c195558e4336fb16b4e3a3c75
Ricardo Dominguez <ricardo@thoughtcatalog.com>
2022-03-31 16:08:02 -0400

    shop@0.1.0-beta04

--- blocks/shop/shop.php
+++ blocks/shop/shop.php
@@ -4,7 +4,7 @@
  * Description:       Dynamic Block which renders a block of Shop Catalog Products.
  * Requires at least: 5.8
  * Requires PHP:      7.0
- * Version:           0.1.0-beta03
+ * Version:           0.1.0-beta04
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
