45f65ad640746efefe08b9258b7794f4dfd5ff34
Ricardo Dominguez <ricardo@thoughtcatalog.com>
2022-04-01 13:32:07 -0400

    remove block utm function from plugin

--- cata-blocks.php
+++ cata-blocks.php
@@ -36,34 +36,3 @@ require_once __DIR__ . '/includes/class-shop-proxy.php';
  * Instantiate Classes
  */
 new Shop_Proxy();
-
-/**
- * Add UTMs to Product Block links
- *
- * @param string $link Link to be modified.
- * @return string $new_link Modified product link.
- */
-function cata_add_product_link_utm( string $link ) {
-	if ( is_front_page() ) {
-		$type = 'frontpage';
-	} elseif ( is_single() && 'post' === get_post_type() ) {
-		$type = 'post';
-	} else {
-		return $link;
-	}
-
-	// get current site name and get rid of any spaces/linebreaks/ and make lowercase
-	$site_name = strtolower( preg_replace("/\s+/", "", get_bloginfo( 'show' ) ) );
-
-	$new_link = add_query_arg( 
-		array(
-			'utm_campaign' => $site_name,
-			'utm_medium'   => 'web',
-			'utm_source'   => $type,
-		),
-		$link
-	);
-
-	return $new_link;
-}
-add_filter( 'cata_product_block_link', __NAMESPACE__ . '\\cata_add_product_link_utm', 10, 1 );
