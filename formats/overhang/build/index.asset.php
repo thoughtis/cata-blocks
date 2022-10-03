f35ab0f9eb011c9e4bdb8849e3eaf53e5612eea8
Jessica Townsend <jessica@thoughtcatalog.com>
2022-10-03 15:23:18 -0400

    overhang format only available for headings

--- formats/overhang/build/index.asset.php
+++ formats/overhang/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => 'dd84396ad5f1c9fa392733ad3fdb47db');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => '8ab3cf2a0b4ac70cd12895ca2cb4ec9c');
--- formats/overhang/src/index.js
+++ formats/overhang/src/index.js
@@ -5,21 +5,30 @@
  import { registerFormatType, toggleFormat } from '@wordpress/rich-text';
  import { RichTextToolbarButton } from '@wordpress/block-editor';
  import { lineSolid } from '@wordpress/icons';
+ import { useSelect } from '@wordpress/data';
  
  const CataOverhangButton = ( { isActive, onChange, value } ) => {
+    const selectedBlock = useSelect( ( select ) => {
+        return select( 'core/block-editor' ).getSelectedBlock();
+    }, [] );
+
+    if ( selectedBlock && selectedBlock.name !== 'core/heading' ) {
+        return null;
+    }
+
     return (
-    <RichTextToolbarButton
-        icon={ lineSolid }
-        title={ __( 'Overhang' ) }
-        onClick={ () => {
-        onChange(
-            toggleFormat( value, {
-                type: 'cata-format/overhang',
-            } )
-        );
-        } }
-        isActive={ isActive }
-    />
+        <RichTextToolbarButton
+            icon={ lineSolid }
+            title={ __( 'Overhang' ) }
+            onClick={ () => {
+            onChange(
+                toggleFormat( value, {
+                    type: 'cata-format/overhang',
+                } )
+            );
+            } }
+            isActive={ isActive }
+        />
     );
  };
  
