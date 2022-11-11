c22d0d3733d36f2eeb2205fdcc137b8fd753d1f4
Jessica Townsend <jessica@thoughtcatalog.com>
2022-11-11 10:41:01 -0500

    compile assets

--- blocks/products/build/index.asset.php
+++ blocks/products/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '53801ba24b2a648560126bd82cc316d5');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'fedc85fdfc30410c5c5717fe6b636411');
