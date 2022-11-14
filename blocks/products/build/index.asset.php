004de7276087e525e22976af511b7059105310f7
Jessica Townsend <jessica@thoughtcatalog.com>
2022-11-14 16:59:24 -0500

    compile assets after rebase with main

--- blocks/products/build/index.asset.php
+++ blocks/products/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'fedc85fdfc30410c5c5717fe6b636411');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1f9e528d8f6d339e69d7598aec8070be');
