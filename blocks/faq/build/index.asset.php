f75a86e4198cd70ac1877855b337600ba37c588a
Jessica Townsend <jessica@thoughtcatalog.com>
2023-05-05 14:39:00 -0400

    style inline faq block

--- blocks/faq/build/index.asset.php
+++ blocks/faq/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '5eaa3c01e2086b69f47177f53d070598');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '5e840bf62f0c355d41b25bdce6f096a5');
