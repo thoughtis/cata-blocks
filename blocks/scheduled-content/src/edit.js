f84d05272454f521d209f7d98a078138c0f992cb
Jessica Townsend <jessica@thoughtcatalog.com>
2023-05-15 13:42:40 -0400

    add date time picker to scheduled content block

--- blocks/scheduled-content/src/edit.js
+++ blocks/scheduled-content/src/edit.js
@@ -2,19 +2,49 @@
  * Edit
  */
 
-import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+import { useBlockProps, InnerBlocks, InspectorControls } from '@wordpress/block-editor';
+import { Panel, PanelBody, PanelRow, DateTimePicker } from '@wordpress/components';
+import { useState } from '@wordpress/element';
+
 
 /**
  * Edit
  * 
  * @export
  * @return {WPElement} Edit
- */
-export default function Edit() {
+*/
+export default function Edit( { attributes, setAttributes } ) {
 	const blockProps = useBlockProps();
+	const { startDate, endDate } = attributes;
 	return (
-		<div { ...blockProps } >
-			<InnerBlocks />
-		</div>
+		<>
+			<InspectorControls>
+				<Panel header="Schedule">
+					<PanelBody title="Start" initialOpen={ true }>
+						<PanelRow>
+							<DateTimePicker
+								currentDate={ startDate || null }
+								onChange={ ( newStartDate ) => setAttributes( {startDate: newStartDate} ) }
+								is12Hour={ true }
+								__nextRemoveHelpButton
+							/>
+						</PanelRow>
+					</PanelBody>
+					<PanelBody title="End" initialOpen={ true }>
+						<PanelRow>
+							<DateTimePicker
+								currentDate={ endDate || null }
+								onChange={ ( newEndDate ) => setAttributes( {endDate: newEndDate} ) }
+								is12Hour={ true }
+								__nextRemoveHelpButton
+							/>
+						</PanelRow>
+					</PanelBody>
+				</Panel>
+			</InspectorControls>
+			<div { ...blockProps } >
+				<InnerBlocks />
+			</div>
+		</>
 	);
 }
