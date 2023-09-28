efbeb2edafaae596f4cfb2e24c1ec93579734597
Jessica Townsend <jessica@thoughtcatalog.com>
2023-09-28 13:21:59 -0700

    reorganize network links edit function

--- blocks/network-links/src/edit.js
+++ blocks/network-links/src/edit.js
@@ -1,38 +1,42 @@
 /**
- * WordPress dependencies
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-block-editor/#useBlockProps
  */
 import { useInnerBlocksProps, useBlockProps, } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 
-const ALLOWED_BLOCKS = [ 'cata/network-link' ];
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
 
-export default function Edit( props ) {
-	const {
-		attributes,
-		isSelected,
-	} = props;
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+*
+* @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+*
+* @return {WPElement} Element to render.
+*/
+export default function Edit() {
+	const ALLOWED_BLOCKS = [ 'cata/network-link' ];
 
 	const NetworkPlaceholder = (
-		<li className="wp-block-network-links__social-placeholder">
-			<div className="wp-block-network-links__social-placeholder-icons">
-				<div className="wp-block-network-link wp-block-network-link-thoughtcatalog"></div>
-				<div className="wp-block-network-link wp-block-network-link-collectiveworld"></div>
-				<div className="wp-block-network-link wp-block-network-link-creepycatalog"></div>
-				<div className="wp-block-network-link wp-block-network-link-shopcatalog"></div>
-			</div>
-		</li>
-	);
-
-	const SelectedNetworkPlaceholder = (
 		<li className="wp-block-cata-network-links__prompt">
 			{ __( 'Click plus to add' ) }
 		</li>
 	);
 
 	const blockProps = useBlockProps();
+
 	const innerBlocksProps = useInnerBlocksProps( blockProps, {
 		allowedBlocks: ALLOWED_BLOCKS,
-		placeholder: isSelected ? SelectedNetworkPlaceholder : NetworkPlaceholder,
+		placeholder: NetworkPlaceholder,
 		templateLock: false,
 		__experimentalAppenderTagName: 'li',
 	} );
