cf6d402dbeff1e6abefb1c18a27aba6ef88df3bc
Jessica Townsend <jessica@thoughtcatalog.com>
2023-09-28 14:15:34 -0700

    update logo svg sizes

--- blocks/network-link/build/index.asset.php
+++ blocks/network-link/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e3fa8438a8be7febb6f1e3ae3b4d2217');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'fde5cbfd468d6940f5528bbf9e051e78');
