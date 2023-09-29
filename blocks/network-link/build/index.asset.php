87ee07877c79802055c352c008f1cab05fcf85e8
Jessica Townsend <jessica@thoughtcatalog.com>
2023-09-29 12:19:27 -0700

    wrap svg paths with a group

--- blocks/network-link/build/index.asset.php
+++ blocks/network-link/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '027c7b034838969bd595973654fafb40');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '84095ca5bade0d689d3c05bcb1b21926');
