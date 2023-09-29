a9a42961969d85f73eed5e21186d69b4dffa57bb
Jessica Townsend <jessica@thoughtcatalog.com>
2023-09-29 07:55:30 -0700

    add a width and height to svg logos

--- blocks/network-link/build/index.asset.php
+++ blocks/network-link/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'fde5cbfd468d6940f5528bbf9e051e78');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '027c7b034838969bd595973654fafb40');
