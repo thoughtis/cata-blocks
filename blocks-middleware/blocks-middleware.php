8e962db504b2451f11907261535adfec5d5a75f6
Jessica Townsend <jessica@thoughtcatalog.com>
2023-10-11 12:11:03 -0700

    add dependency and version number for middleware

--- blocks-middleware/blocks-middleware.php
+++ blocks-middleware/blocks-middleware.php
@@ -12,11 +12,12 @@ namespace Cata\Blocks;
  * Enqueue Scripts
  */
 function cata_blocks_middleware_enqueue_scripts() {
-	do_action('qm/debug', 'middleware script enqueued');
+	$asset = require __DIR__ . '/build/index.asset.php';
 	wp_enqueue_script( 
 		'cata-blocks-middleware-module-app', 
-		plugin_dir_url( __FILE__ ) . 'build/index.js', 
-		array()
+		plugin_dir_url( __FILE__ ) . 'build/index.js',
+		$asset['dependencies'],
+		$asset['version']
 	);
 }
 add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\\cata_blocks_middleware_enqueue_scripts' );
