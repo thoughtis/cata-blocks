2d8e066abb4732bffe377a83c601a460ea216408
Jessica Townsend <jessica@thoughtcatalog.com>
2023-11-29 09:48:46 -0800

    update styles to reflect html changes

--- blocks/rest/build/index.asset.php
+++ blocks/rest/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c42e7f101508c03f37e67ab835c07796');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '154800b240b00820de16b90eddbc5f8a');
