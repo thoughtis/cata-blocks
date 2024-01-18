1d82150c3b92be42cd760c90fc6b96955df5450d
Jessica Townsend <jessica@thoughtcatalog.com>
2024-01-18 08:30:10 -0800

    0.8.12

--- cata-blocks.php
+++ cata-blocks.php
@@ -12,7 +12,7 @@
  * Description: Block Editor components for use with the Cata theme.
  * Author:      Thought & Expression Co. <devjobs@thought.is>
  * Author URI:  https://thought.is
- * Version:     0.8.12-beta2
+ * Version:     0.8.12
  * License:     GPL v3 or later
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  */
