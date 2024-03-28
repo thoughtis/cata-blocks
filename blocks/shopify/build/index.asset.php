28c6ae6fc53513ec3bf70f040ecec79a9a1d21b5
Jessica Townsend <jessica@thoughtcatalog.com>
2024-03-28 10:37:56 -0700

    add css for list style

--- blocks/shopify/build/index.asset.php
+++ blocks/shopify/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element'), 'version' => '491dc041e5b5ab228fb2');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element'), 'version' => 'fb0582c980e27e6a9408');
