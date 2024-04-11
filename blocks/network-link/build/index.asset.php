7a3d6e4cf7b35b14a173342c3a2af1a09066c1c7
Jessica Townsend <jessica@thoughtcatalog.com>
2024-04-11 09:13:16 -0700

    add & adjust styles for the network links blocks

--- blocks/network-link/build/index.asset.php
+++ blocks/network-link/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '198019480643c972a64ede5dd5227d95');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '75ee24ebe3cba9be4b93');
