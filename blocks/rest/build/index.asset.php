f4dae597949c4e9b8ceac5a717f45788d4f80364
Jessica Townsend <jessica@thoughtcatalog.com>
2024-04-12 12:56:38 -0700

    restructure rest api block styles

--- blocks/rest/build/index.asset.php
+++ blocks/rest/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9e56f272e1891e322565');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'adcfcc88f9e6c4cb13c4');
