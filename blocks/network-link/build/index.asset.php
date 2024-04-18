aa0dc6990527faf4ae2020adc89f2e76d8c73aaf
Jessica Townsend <jessica@thoughtcatalog.com>
2024-04-18 14:33:49 -0700

    recompile assets after rebase

--- blocks/network-link/build/index.asset.php
+++ blocks/network-link/build/index.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'dacecbd087283d3d175e');
-=======
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '75ee24ebe3cba9be4b93');
->>>>>>> 7a3d6e4 (add & adjust styles for the network links blocks)
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'd945a8d528932060379e');
