977408bb04a11665d1e888ac7384e15a4a005ab0
Jessica Townsend <jessica@thoughtcatalog.com>
2024-05-24 10:40:46 -0700

    update icon for block

--- blocks/marquee/src/index.js
+++ blocks/marquee/src/index.js
@@ -18,5 +18,5 @@ import './style.scss';
 registerBlockType( 'cata/marquee', {
 	edit: Edit,
 	save: Save,
-	icon: <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><rect x="2" y="2" width="20" height="20" fill="none" stroke="black" strokeWidth="2" strokeDasharray="2 2" strokeDashoffset="1" /></svg>
+	icon: <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18.29 17.29a.996.996 0 0 0 0-1.41L14.42 12l3.88-3.88a.996.996 0 1 0-1.41-1.41L12.3 11.3a.996.996 0 0 0 0 1.41l4.59 4.59c.38.38 1.01.38 1.4-.01z"/><path d="M11.7 17.29a.996.996 0 0 0 0-1.41L7.83 12l3.88-3.88a.996.996 0 1 0-1.41-1.41L5.71 11.3a.996.996 0 0 0 0 1.41l4.59 4.59c.38.38 1.01.38 1.4-.01z"/></svg>
 } );
