38248669825e1d2a4f1e7930f5dbce2860326d2b
Jessica Townsend <jessica@thoughtcatalog.com>
2024-07-10 13:37:25 -0700

    use css variable for the stroke color in editor

--- blocks/globe-background/src/edit.js
+++ blocks/globe-background/src/edit.js
@@ -33,7 +33,7 @@ export default function Edit( { attributes, setAttributes } ) {
 			<div
 				{ ...useBlockProps( {
                     style: {
-                        stroke: globeColor,
+                        "--cata-globe-color": globeColor,
                     },
                 } ) }
 			>
