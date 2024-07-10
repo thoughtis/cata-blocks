4e0f76e2402e20c57eb70deb491c2d5f85389122
Jessica Townsend <jessica@thoughtcatalog.com>
2024-07-10 13:37:55 -0700

    remove background color support

--- blocks/globe-background/build/index.asset.php
+++ blocks/globe-background/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'a916d8dab3042aedd1ae');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '7b79f0861d06693adad3');
