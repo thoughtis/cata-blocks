dc98648eea77d72dc1cca97ea80775f46d871a4a
Jessica Townsend <jessica@thoughtcatalog.com>
2024-07-10 14:26:14 -0700

    add globe svg in save function instead of callback

--- blocks/globe-background/build/index.asset.php
+++ blocks/globe-background/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '7b79f0861d06693adad3');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'c4bf50a9376f8e8f99ee');
--- blocks/globe-background/src/save.js
+++ blocks/globe-background/src/save.js
@@ -10,12 +10,28 @@ import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
  * @export
  * @return {WPElement} Save
  */
-export default function Save() {	
+export default function Save( { attributes } ) {	
 	const blockProps = useBlockProps.save();
+	const style = "stroke: " + attributes.globeColor;
 
 	return (
-		<div { ...blockProps }>
-			<InnerBlocks.Content />
+		<div { ...blockProps } >
+			<div className="wp-block-cata-globe-background__globe-svg" style={style}>
+				<svg viewBox="0 0 300 300" xmlns="http://www.w3.org/2000/svg" width="300" height="300">
+					<circle cx="50%" cy="50%" r="50%"></circle>
+					<path d="M150,300A150,150,0,0,1,150,0" style="--globe-animation-order: 0"></path>
+					<path d="M150,300A150,150,0,0,1,150,0" style="--globe-animation-order: 1"></path>
+					<path d="M150,300A150,150,0,0,1,150,0" style="--globe-animation-order: 2"></path>
+					<path d="M150,300A150,150,0,0,1,150,0" style="--globe-animation-order: 3"></path>
+					<path d="M150,300A150,150,0,0,1,150,0" style="--globe-animation-order: 4"></path>
+					<path d="M150,300A150,150,0,0,1,150,0" style="--globe-animation-order: 5"></path>
+					<path d="M150,300A150,150,0,0,1,150,0" style="--globe-animation-order: 6"></path>
+					<path d="M150,300A150,150,0,0,1,150,0" style="--globe-animation-order: 7"></path>
+				</svg>
+			</div>
+			<div class="wp-block-cata-globe-background__inner-blocks">
+				<InnerBlocks.Content />
+			</div>
 		</div>
 	);
 }
