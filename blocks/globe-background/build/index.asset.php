750f0e73aa8acc079d8443f1fc09b916a6125ad3
Douglas Johnson <doug@thoughtcatalog.com>
2024-07-18 13:46:52 -0400

    build recently updated blocks

--- blocks/globe-background/build/index.asset.php
+++ blocks/globe-background/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => 'c4bf50a9376f8e8f99ee');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '18dc08db7c8ba01a6e1c');
