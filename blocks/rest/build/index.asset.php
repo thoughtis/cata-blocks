6911b2ba168fe7fae2708b697a73e801d8915f74
Douglas Johnson <doug@thoughtcatalog.com>
2024-07-23 12:39:33 -0400

    support zodiac links in network layout JS

--- blocks/rest/build/index.asset.php
+++ blocks/rest/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '441c1b3aa9da2b741065');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1d23267e1a3e22aef82b');
