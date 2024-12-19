97fc98f98730b1a2e5afb6d3aa6712870b5205e8
Jessica Townsend <jessica@thoughtcatalog.com>
2024-12-19 14:24:05 -0800

    use width instead of inline-size

--- blocks/reel-clip/build/index.asset.php
+++ blocks/reel-clip/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '3eb12f73c0acc29a8d4b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0a8ddd8f14395966526d');
