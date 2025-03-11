2c3eadc53e880f9719f6e99cd757077bc7b363ae
Jessica Townsend <jessica@thoughtcatalog.com>
2025-03-11 12:28:54 -0700

    only apply styles to modal buttons

--- blocks/advanced-modal-close/build/index.asset.php
+++ blocks/advanced-modal-close/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'b843ae0901db52cc6b94');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'fc5e0be1903707232323');
