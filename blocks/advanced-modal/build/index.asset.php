08a89acdfda983013b5837b9868aebcc4fa50dc9
Jessica Townsend <jessica@thoughtcatalog.com>
2025-03-11 09:25:52 -0700

    make button elements inherit color too

--- blocks/advanced-modal/build/index.asset.php
+++ blocks/advanced-modal/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5784f5bcc706b3bd1998');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c6625866a430cf57c46a');
