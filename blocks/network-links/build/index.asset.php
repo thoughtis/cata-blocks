94d8ef581f57e2b29a9cfc140ee8845ae013f0f4
Jessica Townsend <jessica@thoughtcatalog.com>
2025-04-23 12:37:07 -0700

    build network links block

--- blocks/network-links/build/index.asset.php
+++ blocks/network-links/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8ecb1050bc0d189017db');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7ee21e06237ea654aa4d');
