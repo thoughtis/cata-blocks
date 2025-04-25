69114c0343ff992483c68fe0fc70e3bad9b33d5a
Jessica Townsend <jessica@thoughtcatalog.com>
2025-04-25 07:53:24 -0700

    use className instead of class

--- blocks/rest/build/index.asset.php
+++ blocks/rest/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ed6d4aa74bf011db9ea0');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a9fa2376c00d60b4f75e');
