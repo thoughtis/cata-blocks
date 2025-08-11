f5028d77e07a44a98125a07d6a8d39f187062ccb
Jessica Townsend <jessica@thoughtcatalog.com>
2025-08-11 08:08:37 -0700

    specify that 7search blog is an ad

--- blocks/sevensearch/build/index.asset.php
+++ blocks/sevensearch/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '10a33d5a90dd1e0e0da6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'f994d0a7b5026f968acb');
