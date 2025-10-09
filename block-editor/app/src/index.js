a3437833ca33ff0f3738bbc96b75c21d3c99d018
Douglas Johnson <doug@thoughtcatalog.com>
2025-10-09 15:23:43 -0400

    testing very broad color scheme control

--- block-editor/app/src/index.js
+++ block-editor/app/src/index.js
@@ -3,7 +3,9 @@
  */
 
 import flexGrow from './components/flex-grow';
+import colorScheme from './components/color-scheme';
 import svgUrl from './components/svg-url';
 
+colorScheme();
 flexGrow();
 svgUrl();
