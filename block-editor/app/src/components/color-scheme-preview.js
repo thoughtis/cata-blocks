5ac9fb54bc5c2f0ad32f8d38d2278ac899932fdf
Douglas Johnson <doug@thoughtcatalog.com>
2025-10-16 15:49:27 -0400

    testing hacky version of color scheme preview

---
+++ block-editor/app/src/components/color-scheme-preview.js
@@ -0,0 +1,28 @@
+import { PluginPreviewMenuItem } from '@wordpress/editor';
+import { registerPlugin } from '@wordpress/plugins';
+
+const CustomPreviewMenuItem = () => (
+  <PluginPreviewMenuItem
+    onClick={ () => {
+
+		const siteEditor = document.querySelector('#site-editor');
+		const postEditor = document.querySelector('#editor');
+
+		if ( siteEditor ) {
+			const editorBodyEl = document.querySelector('.edit-site-visual-editor__editor-canvas');
+			editorBodyEl.contentDocument.body.style.colorScheme = 'dark only';
+		}
+
+		if ( postEditor ) {
+			const editorStylesWrapper = document.querySelector('.editor-styles-wrapper');
+			editorStylesWrapper.body.style.colorScheme = 'dark only';
+		}
+    } }
+  >
+    Preview in Dark Mode
+  </PluginPreviewMenuItem>
+);
+
+registerPlugin( 'cata-blocks/color-scheme-preview', {
+    render: CustomPreviewMenuItem,
+} );
--- block-editor/app/src/index.js
+++ block-editor/app/src/index.js
@@ -6,6 +6,8 @@ import flexGrow from './components/flex-grow';
 import colorScheme from './components/color-scheme';
 import svgUrl from './components/svg-url';
 
+import './components/color-scheme-preview.js';
+
 colorScheme();
 flexGrow();
 svgUrl();
