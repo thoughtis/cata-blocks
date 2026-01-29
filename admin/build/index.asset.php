405197b7926f80395fb667c593d7f659ed040c14
Douglas Johnson <doug@thoughtcatalog.com>
2026-01-29 10:08:28 -0500

    add form for admin control of options

---
+++ admin/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-notices'), 'version' => '5bbaf88254a5b7728d0b');
