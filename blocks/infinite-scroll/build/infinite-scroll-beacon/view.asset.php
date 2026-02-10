21042d8958a2881a7a510beae9bd7f1539d33a37
Douglas Johnson <doug@thoughtcatalog.com>
2026-02-10 08:55:28 -0500

    remove now unused view asset

--- blocks/infinite-scroll/build/infinite-scroll-beacon/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '718c26121d59cbedc176', 'type' => 'module');
--- blocks/infinite-scroll/build/infinite-scroll-beacon/view.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello World! (from cata-infinite-scroll block)");
