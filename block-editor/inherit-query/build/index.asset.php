825d4d46e83e17b79ebc1b5312541cfe687dbc08
Jessica Townsend <jessica@thoughtcatalog.com>
2026-06-22 14:02:14 -0700

    use the same panel for both custom query toggles

--- block-editor/inherit-query/build/index.asset.php
+++ block-editor/inherit-query/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-hooks'), 'version' => 'd66fdfde119ddcf66329');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-hooks'), 'version' => '1c3418af34152d311a6d');
