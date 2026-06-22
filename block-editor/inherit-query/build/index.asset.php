1876a484db392f82f95677be46fc0a3a2db095e6
Jessica Townsend <jessica@thoughtcatalog.com>
2026-06-22 15:27:46 -0700

    merge query loop filters into query-filters module

--- block-editor/inherit-query/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-hooks'), 'version' => '1c3418af34152d311a6d');
--- block-editor/term-from-request/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-hooks'), 'version' => 'e925bf369a9934b8d9f4');
