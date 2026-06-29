94b801402e46acc4c648441b7a9b11ad50c92b53
Jessica Townsend <jessica@thoughtcatalog.com>
2026-06-29 14:23:25 -0700

    build assets

--- blocks/image-lightbox/build/index.asset.php
+++ blocks/image-lightbox/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '002b9224ca4654a67f86');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '794ae043aa47478d331d');
---
+++ blocks/image-lightbox/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'd884ce174d45d69ebdea', 'type' => 'module');
